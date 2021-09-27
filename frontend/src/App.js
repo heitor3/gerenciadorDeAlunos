2c53b884ef9245ea3661ed7c3fff8d1d35300f24
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-09-27 18:53:18 -0300

    add css styles and create modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, modalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,6 +14,29 @@ function App() {
 
   const [data, setData] = useState([]);
 
+  const [modalIncluir, setModalIncluir] = useState(false);
+
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: '',
+    nome: '',
+    email: '',
+    idade: ''
+  });
+
+  const abrirFecharModalIncluir = () => {
+    setModalIncluir(!modalIncluir);
+  }
+
+  const handleChange = x => {
+    const { name, valeu } = x.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado, [name]: valeu
+    });
+    console.log(alunoSelecionado);
+  }
+
+
+
   const pedidoGet = async () => {
     await axios.get(baseUrl)
       .then(response => {
@@ -23,17 +46,31 @@ function App() {
       })
   };
 
+  const pedidoPost = async () => {
+    delete alunoSelecionado.id;
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.post(baseUrl, alunoSelecionado)
+      .then(response => {
+        setData(data.concat(response.data));
+        abrirFecharModalIncluir();
+      }).catch(error => {
+        console.log("carai");
+      });
+  };
+
   useEffect(() => {
     pedidoGet();
   });
 
   return (
-    <div className="App">
+    <div className="aluno-container">
       <br />
       <h3>Cadastro de alunos</h3>
       <header>
         <img src={ logoCadastro } alt="cadastro" />
-        <button className="btn btn-success">Incluir Novo Aluno</button>
+        <button className="btn btn-success" onClick={ () => abrirFecharModalIncluir() }>
+          Incluir Novo Aluno
+        </button>
       </header>
       <table className="table table-bordered">
         <thead>
@@ -49,7 +86,7 @@ function App() {
           { data.map(aluno => (
             <tr key={ aluno.id }>
               <td>{ aluno.id }</td>
-              <td>{ aluno.name }</td>
+              <td>{ aluno.nome }</td>
               <td>{ aluno.email }</td>
               <td>{ aluno.idade }</td>
               <td>
@@ -60,6 +97,53 @@ function App() {
           )) }
         </tbody>
       </table>
+      <Modal isOpen={ modalIncluir }>
+        <ModalHeader>Incluir Alunos</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nome: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="nome"
+              onChange={ handleChange }
+            />
+            <br />
+            <label>Email: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              onChange={ handleChange }
+            />
+            <br />
+            <label>Idade: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="idade"
+              onChange={ handleChange }
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button
+            className="btn btn-primary"
+            onClick={ () => pedidoPost() }>
+            Incluir
+          </button>
+          { "  " }
+          <button
+            className="btn btn-danger"
+            onClick={ () => abrirFecharModalIncluir() }>
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
