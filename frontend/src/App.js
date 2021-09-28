02d21cc4f6053ca585fbe9f316fbd5cf6647c11f
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-09-28 19:59:08 -0300

    fix error handleChange and create modal edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ function App() {
 
   const [modalIncluir, setModalIncluir] = useState(false);
 
+  const [modalEditar, setModalEditar] = useState(false);
+
   const [alunoSelecionado, setAlunoSelecionado] = useState({
     id: '',
     nome: '',
@@ -23,19 +25,26 @@ function App() {
     idade: ''
   });
 
+  const selecionarAluno = (aluno, opcao) => {
+    setAlunoSelecionado(aluno);
+    (opcao === "Editar") &&
+      abrirFecharModalEditar();
+  }
+
   const abrirFecharModalIncluir = () => {
     setModalIncluir(!modalIncluir);
   }
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar);
+  }
 
   const handleChange = x => {
-    const { name, valeu } = x.target;
+    const { name, value } = x.target;
     setAlunoSelecionado({
-      ...alunoSelecionado, [name]: valeu
+      ...alunoSelecionado, [name]: value
     });
     console.log(alunoSelecionado);
   }
-
-
 
   const pedidoGet = async () => {
     await axios.get(baseUrl)
@@ -54,9 +63,31 @@ function App() {
         setData(data.concat(response.data));
         abrirFecharModalIncluir();
       }).catch(error => {
-        console.log("carai");
+        console.log(error);
       });
   };
+
+  const pedidoPut = async () => {
+    console.log("botão")
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.put(baseUrl + "/" + alunoSelecionado.id, alunoSelecionado)
+      .then(response => {
+        var resposta = response.data;
+        var dadosAuxiliar = data;
+        console.log("data" + data)
+
+        dadosAuxiliar.map(aluno => {
+          if (aluno.id === alunoSelecionado.id) {
+            aluno.nome = resposta.nome;
+            aluno.email = resposta.email;
+            aluno.idade = resposta.idade;
+          }
+        });
+        abrirFecharModalEditar();
+      }).catch(error => {
+        console.log(error);
+      })
+  }
 
   useEffect(() => {
     pedidoGet();
@@ -90,8 +121,17 @@ function App() {
               <td>{ aluno.email }</td>
               <td>{ aluno.idade }</td>
               <td>
-                <button className="btn btn-primary">Editar</button> { "  " }
-                <button className="btn btn-danger">Excluir</button>
+                <button
+                  className="btn btn-primary"
+                  onClick={ () => selecionarAluno(aluno, "Editar") }>
+                  Editar
+                </button>
+                { "  " }
+                <button
+                  className="btn btn-danger"
+                  onClick={ () => selecionarAluno(aluno, "Excluir") }>
+                  Excluir
+                </button>
               </td>
             </tr>
           )) }
@@ -140,6 +180,67 @@ function App() {
           <button
             className="btn btn-danger"
             onClick={ () => abrirFecharModalIncluir() }>
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={ modalEditar }>
+        <ModalHeader>Editar Aluno</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>ID: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              readOnly value={ alunoSelecionado && alunoSelecionado.id }
+            />
+            <br />
+            <label>Nome: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="nome"
+              onChange={ handleChange }
+              value={ alunoSelecionado && alunoSelecionado.nome }
+            />
+            <br />
+            <label>Email: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              onChange={ handleChange }
+              value={ alunoSelecionado && alunoSelecionado.email }
+            />
+            <br />
+            <label>Idade: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="idade"
+              onChange={ handleChange }
+              value={ alunoSelecionado && alunoSelecionado.idade }
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button
+            className="btn btn-primary"
+            onClick={ () => pedidoPut() }
+          >
+            Editar
+          </button>
+          { "  " }
+          <button
+            className="btn btn-danger"
+            onClick={ () => abrirFecharModalEditar() }
+          >
             Cancelar
           </button>
         </ModalFooter>
