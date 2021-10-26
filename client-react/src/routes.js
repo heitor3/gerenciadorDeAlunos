8b97be3ad67a1d4e314671c7d512852fd66e3b17
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-10-25 22:21:11 -0300

    create NovoAluno component

--- client-react/src/routes.js
+++ client-react/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Alunos from './pages/Alunos';
+import NovoAluno from './pages/NovoAluno';
 
 export default function Routes() {
   return (
@@ -9,6 +10,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={ Login } />
         <Route path="/alunos" component={ Alunos } />
+        <Route path="/aluno/novo/:alunoId" component={ NovoAluno } />
       </Switch>
     </BrowserRouter>
   );
